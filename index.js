b4c004def8e9dbebf777f99482fb63b09ea89089
CatzillaOrz <catzillaorz@outlook.com>
2022-08-05 15:17:49 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // Import stylesheets
 
 import './style.css';
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAge(1991);
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1991);
