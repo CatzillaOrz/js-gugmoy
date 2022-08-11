3d06e055e268798b5c5f88b86f63b07eb8ef13c5
CatzillaOrz <catzillaorz@outlook.com>
2022-08-11 16:29:03 +0800

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // DOM lifecycle
+console.log('helleo');
 
 document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('e');
+  console.log(e);
+  console.log('asdfadf');
 });
+let node = document.createElement('h1');
+node.innerText = 'hell';
+document.querySelector('body').appendChild(node);
+document
+  .querySelector('body')
+  .insertAdjacentHTML('beforeend', `<h1>Hello world</h1>`);
