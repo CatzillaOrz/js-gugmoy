e26a9717ea363953d9be8e03d70166dcf575b56a
CatzillaOrz <catzillaorz@outlook.com>
2022-08-12 15:09:23 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 import './style.css';
+
+/*
+ **  How Prototypal Inheritance / Delegation works
+ **
+ **
+ */
+
+class Person {
+  name;
+  age;
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+console.log(Person.prototype); //  : {constructor: ƒ}
+console.log(Person.prototype.constructor); // :
+/* class Person {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+} */
