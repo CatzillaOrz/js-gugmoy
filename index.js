6aa0649749bf9347188a93cbd5ea730960cf88d9
CatzillaOrz <catzillaorz@outlook.com>
2022-08-12 16:55:09 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+const PersonProto = {
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+steven.name = 'Steven';
+steven.birthYear = 1998;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+console.log(steven instanceof Object);
