07ffd42b58ab1b1b5c709b42bb7e485080bb5cab
CatzillaOrz <catzillaorz@outlook.com>
2022-08-12 14:39:31 +0800

    1 file modified, 1 added

---
+++ lifecycle.js
@@ -0,0 +1,24 @@
+// DOM lifecycle
+
+console.log('helleo');
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log(e);
+  console.log('asdfadf');
+});
+let node = document.createElement('h1');
+node.innerText = 'hell';
+document.querySelector('body').appendChild(node);
+document
+  .querySelector('body')
+  .insertAdjacentHTML('beforeend', `<h1>Hello world</h1>`);
+
+window.addEventListener('load', function (e) {
+  console.log(e);
+});
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  // console.log(e);
+  e.returnValue = 'Message is showing @catzilla';
+});
