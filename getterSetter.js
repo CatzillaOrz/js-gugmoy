9d8c7e2c35fdf1c1077cd70f11d87026e95a4dd4
CatzillaOrz <catzillaorz@outlook.com>
2022-08-12 16:27:32 +0800

    1 file modified, 1 added

---
+++ getterSetter.js
@@ -0,0 +1,35 @@
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 300, 120, 300],
+
+  get latest() {
+    return this._latest;
+  },
+
+  set latest(val) {
+    this._latest = val;
+  },
+};
+account.latest = 299;
+console.log(account);
+console.log(account.latest);
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  set fullName(name) {
+    this._fullName = name;
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const Lily = new PersonCl('Lily', 1982);
+console.log(Lily);
+console.log(Lily.fullName);
