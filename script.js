da1e2f6c9abac2308a9ff9262358d7d68c796fd3
catzillaorz <catzillaorz@outlook.com>
2022-08-22 16:09:39 +0800

    feat: Modules

---
+++ script.js
@@ -0,0 +1,8 @@
+// importing  module
+
+// import { totalPrice, totalQuantity } from './shoppingCarts.js';
+import * as ShoppingCart from './shoppingCarts.js';
+console.log('Imported Shopping Cart');
+
+console.log('My totalPrice is', ShoppingCart.totalPrice);
+console.log('My totalQuantity is', ShoppingCart.totalQuantity);
