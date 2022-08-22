d233d6d72ef4c98ba97f8516c32bc374105688b0
catzillaorz <catzillaorz@outlook.com>
2022-08-22 16:27:33 +0800

    feat: top level await

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // importing  module
 
 // import { totalPrice, totalQuantity } from './shoppingCarts.js';
-import * as ShoppingCart from './shoppingCarts.js';
-console.log('Imported Shopping Cart');
+// import * as ShoppingCart from './shoppingCarts.js';
+// console.log('Imported Shopping Cart');
 
-console.log('My totalPrice is', ShoppingCart.totalPrice);
-console.log('My totalQuantity is', ShoppingCart.totalQuantity);
+// console.log('My totalPrice is', ShoppingCart.totalPrice);
+// console.log('My totalQuantity is', ShoppingCart.totalQuantity);
+
+/**
+ * Top level await defer script In JS await async
+ *
+ * @type {*} */
+/*
+// await async
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+const data = await res.json();
+console.log(data);
+ */
